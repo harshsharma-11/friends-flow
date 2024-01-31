9709b9d0b5c2097b4ef3018f3e452ae58987151a
Harsh Sharma <harshsharma90153@gmail.com>
2024-01-31 12:52:16 +0530

    profile updates responsiveness

--- ff-frontend/src/timeline/posts/create-post.jsx
+++ ff-frontend/src/timeline/posts/create-post.jsx
@@ -20,7 +20,7 @@ function CreatePost() {
       navigate('/login');
     }
     else{
-      setUser(JSON.parse(localStorage.getItem('user')).username);
+      setUser(JSON.parse(localStorage.getItem('user')));
     }
   }, []); // Make sure to include the dependency array
 
@@ -94,9 +94,9 @@ body:JSON.stringify({
         <div className="create-post-footer"> 
         <div className="create-post-user">
             <div className="create-post-user-image">
-                <img src="https://plus.unsplash.com/premium_photo-1664474619075-644dd191935f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MXx8aW1hZ2V8ZW58MHx8MHx8fDA%3D&w=1000&q=80"></img>
+                <img src={user.profile_pic}></img>
             </div>
-            <div className="create-post-user-name">{user}</div>
+            <div className="create-post-user-name">{user.username}</div>
         </div>
         <div className="create-post-caption">
           <textarea onChange={(e)=>{
